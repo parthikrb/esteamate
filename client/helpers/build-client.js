4c5ad9c204ef883ae70805303a995d599e01c1db
parthikrb <parthi.krb@gmail.com>
2020-07-26 11:36:22 +0530

    updated ingress service name

--- client/helpers/build-client.js
+++ client/helpers/build-client.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://ingress-nginx.ingress-nginx.svc.cluster.local",
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
