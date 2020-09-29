863dda9c6b79223b22cbaddbec274a8793fff236
Parthiban Baskar <parthi.krb@gmail.com>
2020-09-29 12:10:22 +0530

    demo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,46 @@
-import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import { json } from 'body-parser';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import { json } from "body-parser";
 
 // Set Cookie
-import cookieSession from 'cookie-session';
+import cookieSession from "cookie-session";
 
 // DB
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // Routes
-import { SignupRouter } from './routes/signup';
-import { SigninRouter } from './routes/signin';
-import { SignoutRouter } from './routes/signout';
-import { CurrentuserRouter } from './routes/current-user';
-import { showUserRouter } from './routes/show';
+import { SignupRouter } from "./routes/signup";
+import { SigninRouter } from "./routes/signin";
+import { SignoutRouter } from "./routes/signout";
+import { CurrentuserRouter } from "./routes/current-user";
+import { showUserRouter } from "./routes/show";
 
 // Error Handlers
-import { NotFoundError, errorHandler, DatabaseConnectionError, currentUser, natsWrapper } from '@parthikrb/common'
+import {
+  NotFoundError,
+  errorHandler,
+  DatabaseConnectionError,
+  currentUser,
+  natsWrapper,
+} from "@parthikrb/common";
 
 const app = express();
 app.use(cors());
 
-app.set('trust proxy', true) // trust first proxy
+app.set("trust proxy", true); // trust first proxy
 
 app.use(json());
 
 // Set Cookie in response
-app.use(cookieSession({
-    name: 'session',
-    secure: true,
+app.use(
+  cookieSession({
+    name: "session",
+    secure: false,
     signed: false,
-    maxAge: 24 * 60 * 60 * 1000
-}));
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 
 app.use(currentUser);
 // Routes
@@ -42,61 +50,59 @@ app.use(SignoutRouter);
 app.use(SignupRouter);
 app.use(showUserRouter);
 
-app.all('*', (req, res) => {
-    throw new NotFoundError();
-})
+app.all("*", (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
-
 const startApp = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO URI should be defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
 
-    if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined');
-    }
-    if (!process.env.MONGO_URI) {
-        throw new Error('MONGO URI should be defined');
-    }
-    if (!process.env.NATS_CLIENT_ID) {
-        throw new Error('NATS_CLIENT_ID must be defined');
-    }
-    if (!process.env.NATS_URL) {
-        throw new Error('NATS_URL must be defined');
-    }
-    if (!process.env.NATS_CLUSTER_ID) {
-        throw new Error('NATS_CLUSTER_ID must be defined');
-    }
-
-
-    try {
-
-        await natsWrapper.connect(
-            process.env.NATS_CLUSTER_ID!,
-            process.env.NATS_CLIENT_ID!,
-            process.env.NATS_URL!
-        );
-        natsWrapper.client.on('close', () => {
-            console.log('NATS connection closed!');
-            process.exit();
-        });
-        process.on('SIGINT', () => natsWrapper.client.close());
-        process.on('SIGTERM', () => natsWrapper.client.close());
-
-        await mongoose.connect(process.env.MONGO_URI!, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true
-        }).catch(err => {
-            console.error(err.message);
-            throw new DatabaseConnectionError(err.message);
-        });
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error(err.message);
-    }
-    app.listen(3000, () => {
-        console.log(`Listening on PORT 3000`);
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
     });
-}
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose
+      .connect(process.env.MONGO_URI!, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      })
+      .catch((err) => {
+        console.error(err.message);
+        throw new DatabaseConnectionError(err.message);
+      });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err.message);
+  }
+  app.listen(3000, () => {
+    console.log(`Listening on PORT 3000`);
+  });
+};
 
 startApp();
